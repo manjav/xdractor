87f19087fceb180aaca5c1351549bb4bfe7b77db
Mansour <mansurjavadi@gmail.com>
2019-10-03 22:47:42 +0330

    manifest pars and create map of export  files

--- src/Main.hx
+++ src/Main.hx
@@ -1,3 +1,5 @@
+import haxe.format.JsonParser;
+import haxe.zip.Entry;
 import haxe.zip.Reader;
 import haxe.Timer;
 
@@ -25,7 +27,40 @@ class Main
 		var input = sys.io.File.read(_source);
 		var entries = new Reader(input).read();
 		input.close();
+
+		var files = readManifest(entries);
 		
-		trace(Timer.stamp() - now);
+		trace(Timer.stamp() - now, files);
+	}
+	static function readManifest(entries:List<Entry>):Map<String, String>
+	{
+		var ret = new Map<String, String>();
+		for (e in entries)
+		{
+			if( e.fileName == "manifest" )
+			{
+				var data = Reader.unzip(e);
+				var json = JsonParser.parse(data.getString(0,data.length));
+				var children:Array<Dynamic> = json.children;
+				for (c in children)
+				{
+					if( c.name == "artwork" )
+					{
+						var artworks:Array<Dynamic> = c.children;
+						for (a in artworks)
+							if( a.name != "pasteboard" )
+								ret.set(_destination + "/" + a.name + ".json", "artwork/" + a.path + "/graphics/graphicContent.agc");
+					}
+					else if( c.name == "resources" )
+					{
+						var resources:Array<Dynamic> = c.components;
+						for (r in resources)
+							if( r.type == "image/png" )
+								ret.set( _destination + "/images/" + r.path + ".png", "resources/" + r.path);
+					}
+				}
+			}
+		}
+		return ret;
 	}
 }
