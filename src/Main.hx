aadb00d0b6c3fa38a9744cdfcca91e31953cbff5
Mansour <mansurjavadi@gmail.com>
2019-10-03 22:40:06 +0330

    args recievd and test

--- src/Main.hx
+++ src/Main.hx
@@ -1,5 +1,21 @@
-class Main {
-	static function main() {
-		trace("Hello, world!");
+class Main
+{
+	static var _source:String = "map.xd";
+	static var _destination:String = "export";
+	static function main()
+	{
+		// receive args
+		var args = Sys.args();
+		for( i  in 0...args.length )
+		{
+			if( args[i].substring(0, 1) != "-" )
+				continue;
+			switch( args[i] )
+			{
+				case "-src":	_source = args[i + 1];			break;
+				case "-dst":  _destination = args[i + 1];	break;
+			}
+		}
+		trace(args);
 	}
 }
