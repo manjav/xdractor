37506dc1df8069e500f6bf3b31e18f17482d57f1
Mansour <mansurjavadi@gmail.com>
2019-10-03 22:58:13 +0330

    cleaned up

--- src/Main.hx
+++ src/Main.hx
@@ -26,10 +26,12 @@ class Main
 
 		var now = Timer.stamp();
 
+		// load xd file
 		var input = sys.io.File.read(_source);
 		var entries = new Reader(input).read();
 		input.close();
 
+		// manifest reaading
 		var files = readManifest(entries);
 		for (key => value in files)
 			for (e in entries)
@@ -43,27 +45,26 @@ class Main
 		var ret = new Map<String, String>();
 		for (e in entries)
 		{
-			if( e.fileName == "manifest" )
+			if( e.fileName != "manifest" )
+				continue;
+			var data = Reader.unzip(e);
+			var json = JsonParser.parse(data.getString(0,data.length));
+			var children:Array<Dynamic> = json.children;
+			for (c in children)
 			{
-				var data = Reader.unzip(e);
-				var json = JsonParser.parse(data.getString(0,data.length));
-				var children:Array<Dynamic> = json.children;
-				for (c in children)
+				if( c.name == "artwork" )
 				{
-					if( c.name == "artwork" )
-					{
-						var artworks:Array<Dynamic> = c.children;
-						for (a in artworks)
-							if( a.name != "pasteboard" )
-								ret.set(_destination + "/" + a.name + ".json", "artwork/" + a.path + "/graphics/graphicContent.agc");
-					}
-					else if( c.name == "resources" )
-					{
-						var resources:Array<Dynamic> = c.components;
-						for (r in resources)
-							if( r.type == "image/png" )
-								ret.set( _destination + "/images/" + r.path + ".png", "resources/" + r.path);
-					}
+					var artworks:Array<Dynamic> = c.children;
+					for (a in artworks)
+						if( a.name != "pasteboard" )
+							ret.set(_destination + "/" + a.name + ".json", "artwork/" + a.path + "/graphics/graphicContent.agc");
+				}
+				else if( c.name == "resources" )
+				{
+					var resources:Array<Dynamic> = c.components;
+					for (r in resources)
+						if( r.type == "image/png" )
+							ret.set( _destination + "/images/" + r.path + ".png", "resources/" + r.path);
 				}
 			}
 		}
@@ -72,13 +73,14 @@ class Main
 
 	static function save (e:Entry, path:String):Void
 	{
+		// decompress compressed files
 		var data = e.compressed ? Reader.unzip(e) : e.data;
-		// trace(e.fileName, path);
+		
+		// create folders
 		if( !FileSystem.exists(_destination) )
 			FileSystem.createDirectory(_destination);
-		if( !FileSystem.exists(_destination + "/images") )
+		if( path.indexOf("images/") > -1 && !FileSystem.exists(_destination + "/images") )
 			FileSystem.createDirectory(_destination +  "/images");
 		File.saveBytes(path, data);
 	}
-
 }
